c9ed751daba9637369181d96084db0952fb14968
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-06-30 12:01:56 -0700

    Size is not

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));

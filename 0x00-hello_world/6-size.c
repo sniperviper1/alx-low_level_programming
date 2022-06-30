1f56aa3b1d50842661e5cc46b30204deb1445637
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-06-30 13:05:07 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,19 @@
  *
  * Reutrn: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
-}
+	printf("sizeof char is = %i byte\n", sizeof(char));
+	printf("sizeof signed char is = %i byte\n", sizeof(signed char));        	printf("sizeof unsigned char is = %i byte\n\n\n", sizeof(unsigned char));       	
+	printf("sizeof int is = %i byte\n", sizeof(int));
+	printf("sizeof signed is = %i byte\n", sizeof(signed int));
+	printf("sizeof unsigned is = %i byte\n\n\n", sizeof(unsigned int));
+
+	printf("sizeof long int is = %i byte\n", sizeof(long int));
+	printf("sizeof signed long int = %i byte\n", sizeof(signed long int));
+	printf("sizeof unsiged long int = %i byte\n\n\n", sizeof(unsigned long int));
+
+	printf("size of float is = %i byte\n", sizeof(float));
+	printf("sizeof doible is = %i byte\n", sizeof(double));
+	printf("sizeof long double is = %i byte\n\n\n", sizeof(long double));
+	return (0);

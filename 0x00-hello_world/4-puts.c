e583c2efcecb024d7c87d7f8e2c63cba9b11a8e6
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-06-30 07:49:32 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main-Entry point
  *
- * Return 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts( "Programming is like building a multilingual puzzle" "\n" );
 return (0);
 }

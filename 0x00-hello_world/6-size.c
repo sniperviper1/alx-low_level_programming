e27f5588fdaeacf1140ebd743d761ed63218ba4b
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-01 10:34:00 -0700

    Size is mot grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 
 /**
-* main-Entry point
-*
-* Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-printf("sizeof a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("sizeof a int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("sizeof a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("sizeof a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("sizeof a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }

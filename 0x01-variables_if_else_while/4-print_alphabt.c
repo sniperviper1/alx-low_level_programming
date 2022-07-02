3e9afa0154eed1e3191a59365285d12c98a46d22
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-01 19:37:33 -0700

    When i was having that alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: 'Print the alphabet in lowercase, except q and e.'
+ * Description: 'Print the alphabet in lowercase, except q and e.
  *
  * Return: Always 0 (Success)
  */
@@ -13,7 +13,7 @@ char letter;
 
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-if (letter != 'e' && letter != 'q';)
+if (letter != 'e'; letter != 'q')
 putchar(letter);
 }
 putchar('\n');

1562e54f1a66842cc768d4a0e0acb9741cb3edf0
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-01 17:49:25 -0700

    I somtimes sufer from insomnia

---
+++ 2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch < 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+return (0);
+}

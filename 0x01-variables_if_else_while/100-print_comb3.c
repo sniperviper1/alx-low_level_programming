b02533ef1961b72a7a6d94805d24c12a0d00f562
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-03 14:14:01 -0700

    inventing is a combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,9 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: ' prints all possible different combinations of two 
+ * Description: 'prints all possible different combinations of
+ * two
  * digits'
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,16 +15,21 @@ int digit1, digit2;
 
 for (digit1 = 0; digit1 < 9; digit1++)
 {
-for (digit2 = digit1 +1; digit2 < 10; digit2++)
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
+
 putchar((digit1 % 10) + '0');
-putchar((digit2 % 10)+ '0');
+putchar((digit2 % 10) + '0');
+
 if (digit1 == 8 && digit2 == 9)
-	continue;
+
+continue;
+
 putchar(',');
-putchar(" ");
+putchar(' ');
 }
 }
+
 putchar('\n');
 
 return (0);

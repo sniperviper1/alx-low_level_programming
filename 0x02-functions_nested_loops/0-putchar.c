fc1f5024f151e045f8f419ecd9b4077be721a787
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-05 21:19:21 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* printputchar - print '_putchar'
+*
+* Return: nothing
+*/
+void printputchar(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My second function and nested loops assignment

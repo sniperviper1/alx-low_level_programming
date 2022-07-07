fa32fe6af1a8867f487ef6a4a626d035b1dee97f
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-07 05:40:34 -0700

    Multiple main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main -> tests function that print if interger is negative or positive
+* Return; Always 0
+*/
+int main(void)
+{
+int i;
+
+i = 98;
+positive_or_negative(i);
+
+return (0);
+}

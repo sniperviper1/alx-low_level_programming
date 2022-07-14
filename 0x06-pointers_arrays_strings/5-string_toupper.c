c1fe598000ec53598326aac4411266f4983ea213
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-14 12:04:49 -0700

    string toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* string_toupper - converting any string to upper case
+* @x: string para
+*
+* Return: string
+*/
+char *string_toupper(char *x)
+{
+int i = 0;
+
+while (x[i])
+{
+if (x[i] >= 97 && x[i] <= 122)
+x[i] = x[i] - 32;
+i++;
+}
+return (x);
+}

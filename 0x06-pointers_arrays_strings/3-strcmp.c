3fc0e716e1d348c9539567286d68ac9ed595dd02
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-14 10:41:26 -0700

    3strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,6 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int result;
-result = strcmp(s1, s2);
-printf("strcmp(s1, s2) %d\n", result);
-{
-result = strcmp(s2, s1);
-printf("strcmp(s2, s1) %d\n", result);
-{
-result = strcmp(s1, s1);
-printf("strcmp(s1, s1) %d\n", result);
-}
+printf("%d\n", strcmp(s1, s2));
 return (0);
-}
 }

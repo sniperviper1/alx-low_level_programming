a96f47128cdff4f4f0ed3d19409da19a5c7a1cf8
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-15 09:32:54 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,19 +2,20 @@
 
 /**
 * _strcmp - a function to compare two strings
-* @s1: first parameter
-* @s2: second parameter
-* Return: an integer
+* @s1: first string
+* @s2: second string
+* Return: 0 if the string1 and string2 are not same
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
-		while (op == 0)
-		{
-			if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-					break;
-					op = *(s1 + 1) - *(s2 + 1);
-					i++;
-		}
+int i = 0, op = 0;
+
+while (op == 0)
+{
+if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+break;
+op = *(s1 + i) - *(s2 + i);
+i++;
+}
 return (op);
 }

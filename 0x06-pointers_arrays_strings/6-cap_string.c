5cdb4f7b64461d61114dc4945b234e24b5206bd6
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-14 21:36:18 -0700

    capstring

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,26 +2,25 @@
 
 /**
 * cap_string - capitalization
-* @x: string para
-* Return: Capitalizing version of the string
+* @x: string param
+* Return: capitalized version of the string
 */
 char *cap_string(char *x)
 {
-char spc[] = {39, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-int len = 13;
-int a = 0, i;
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int len = 13;
+	int a = 0, i;
 
-while (x[a])
-{
-i = 0;
-while (i < len)
-{
-if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
-x[a] = x[a] - 32;
-i++;
-}
-a++;
-}
+	while (x[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+				i++;
+		}
+		a++;
+	}
+	return (x);
 
-return (x);
 }

259ab7c2daa51ac6dfd6bf30f94fc6c87a8c62ae
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-18 16:28:08 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* set_string - print a value of a pointer
+* @s: parameter 1
+* @to: parameter 2
+* Return: a value
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}

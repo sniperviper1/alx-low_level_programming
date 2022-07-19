11e30031bbf83b83cb13a50c41f7488cb7149714
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-19 13:09:53 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,6 +10,8 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	memcpy(dest, src, n + 1);
-	return (dest);
+	unsigned int i;
+		for (i = 0; i < n; i++)
+			dest[i] = src[i];
+		return (dest);
 }

e6a0b844387d237049ee036c089f9c79dacc9f1b
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-18 23:03:22 -0700

    chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void set_string(char **s, char *to);
-
+void print_diagsums(int *a, int size);
 
 
 #endif

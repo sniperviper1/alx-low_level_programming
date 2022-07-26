5aaf5da955d8acbe376e6cb68189f5ad250e2d28
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-26 01:02:08 -0700

    hot fix on strspn

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,38 +1,33 @@
 #include "main.h"
 
-
-
 /**
-* _strspn - get the length of prifix
-* @s: initial segment
-* @accept: accept bytes
-* Return: the number of byte accepted
+* _strspn - gets the length of a prefix substring
+* @s: string to be scanned
+* @accept: string containing characters to match
+* Return: number of matching characters in the initial segment
 */
-
 unsigned int _strspn(char *s, char *accept)
-
 {
+	int i, j, flag;
+	int count = 0;
 
-	unsigned int i, j, bool;
-
-	for (j = 0; *(s + i) != '\0'; i++)
-
-	{
-
-		bool = 1;
-
-	for (j = 0; *(accept + j) != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 
 		{
 
-	if (*(s + i) == *(accept + j))
-		bool = 0;
-	break;
+		flag = 0;
+
+	for (j = 0; accept[j] != '\0'; j++)
+		{
+		if (s[i] == accept[j])
+			{
+			count++;
+		flag = 1;
+			}
 		}
+	if (flag == 0)
+	return (count);
+		}
+		return (0);
 
-	if (bool == 1)
-	break;
-	}
-
-	return (i);
 }

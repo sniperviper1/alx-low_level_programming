d1aedd4c553760d7eba8729ea44c5c1b761268e3
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-26 08:14:20 -0700

    1 args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - a function to print an integer
+* @argc: first parameter
+* @argv: an array pointer
+* Return: Always 0 on success
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc -1);
+	return (0);
+}

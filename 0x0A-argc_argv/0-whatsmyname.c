528d3553c175227c574f69f202a44430ec7c8329
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-26 07:01:01 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - a function to print it name
+* @argc: first parameter
+* @argv: an array
+* Return: Always 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}

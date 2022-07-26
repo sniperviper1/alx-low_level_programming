787bbc8efe88bfa5d6704f4490ac7399ac480cf5
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-25 23:38:06 -0700

    libmy.a

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - a function to describe the printout
+ * Return: 0
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t - Kevin Mitnick");
+	return (0);
+}

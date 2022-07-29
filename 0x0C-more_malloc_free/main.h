0cbc315d4446f3964f75d9125b56696d2bca29aa
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-07-29 16:36:20 -0700

    prototype added

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,9 +6,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _strlen(char *s);
 int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+int is_digit(char *s);
+int main(int argc, char *argv[]);
 
 
 #endif

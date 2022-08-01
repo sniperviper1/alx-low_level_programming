5302c4e1353f013ca54b54e401401f02f7bc3772
sniperviper1 <dn326045@gmail.com>
2022-08-02 01:37:16 +0800

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+* main - print a name of a file
+* Return: Always 0 on success
+*/
+int main(void)
+{
+    printf("%s\n", __FILE__);
+    return (0);
+}

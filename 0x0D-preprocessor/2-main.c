bb6926da1b2f9dab6596967d1e22958e8caf23b6
Daniel Kwak Anyigbanya Nelson <dn326045@gmail.com>
2022-08-01 19:13:21 -0700

    file name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
-<<<<<<< HEAD
 * main - function to print a file
 * Return: Always 0 (Success)
 */
@@ -10,13 +9,3 @@ int main(void)
 	printf("%s\n", __FILE__);
 	return (0);
 }
-=======
-* main - print a name of a file
-* Return: Always 0 (success)
-*/
-int main(void)
-{
-    printf("%s\n", __FILE__);
-    return (0);
-}
->>>>>>> 2e0376f12da2dc53f6eb8a58151335833eead7eb
